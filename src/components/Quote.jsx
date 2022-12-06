49d72e923f1504d27c55b386967e87dfff760606
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-07 00:25:11 +0500

    Fix quote layout

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../css/Quote.css';
 
 const Quote = () => (
-  <h2>
+  <h2 className="quote">
     &quot;Mathematics is not about numbers, equations, computations or algorithms:
     it is about understanding.&quot; -William Paul Thurston
   </h2>
