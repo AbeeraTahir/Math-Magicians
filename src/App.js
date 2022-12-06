d5c00e7a60cf243995f706f48d75b7f1b2ad2329
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-06 22:12:41 +0500

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
-const App = () => <Calculator />;
+const App = () => (
+  <>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+      </Routes>
+    </Router>
+  </>
+);
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Navbar.css';
+
+const Navbar = () => (
+  <nav>
+    <h1>Math Magicians</h1>
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quote</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
