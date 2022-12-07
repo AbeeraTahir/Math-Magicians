57791b5724442122372b97cf8f1b0a0c76ad8315
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-08 00:41:30 +0500

    Add tests for react components

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
+
+it('Calculator component should render correctly', () => {
+  const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
