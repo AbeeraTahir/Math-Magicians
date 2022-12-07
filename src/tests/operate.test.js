f3a22cc56180e80c5c7c3a14e3a2697c3a0e6dda
amexabee <amexabee@yahoo.com>
2022-12-07 22:21:48 +0200

    Add more tests

---
+++ src/tests/operate.test.js
@@ -0,0 +1,36 @@
+import operate from '../logic/operate';
+
+describe('Test for operation', () => {
+  it('addition', () => {
+    const result = operate('1', '2', '+');
+    expect(result).toBe('3');
+  });
+
+  it('subtraction', () => {
+    const result = operate('5', '2', '-');
+    expect(result).toBe('3');
+  });
+  it('multiplication', () => {
+    const result = operate('5', '2', 'x');
+    expect(result).toBe('10');
+  });
+  it('division', () => {
+    const result = operate('5', '2', '÷');
+    expect(result).toBe('2.5');
+  });
+  it('division by zero', () => {
+    const result = operate('5', '0', '÷');
+    expect(result).toBe("Can't divide by 0.");
+  });
+  it('modulus', () => {
+    const result = operate('5', '2', '%');
+    expect(result).toBe('1');
+  });
+  it('modulus by zero', () => {
+    const result = operate('5', '0', '%');
+    expect(result).toBe("Can't find modulo as can't divide by 0.");
+  });
+  it('Invalid operation', () => {
+    expect(() => operate('5', '2', '@')).toThrow();
+  });
+});
